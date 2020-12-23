4857562547b715653648dc4bb7fe046d3c26277f
chili <1032569304@qq.com>
2020-12-23 14:55:09 +0800

    完成 account 页面顶部 UI

--- src/views/Account.tsx
+++ src/views/Account.tsx
@@ -1,8 +1,30 @@
 import React from "react";
 import Layout from "components/Layout";
+import styled from "styled-components";
+import Icon from "../components/Icon";
+
+const Topbar = styled.section`
+  display:flex;
+  justify-content: space-between;
+  align-items: center;
+  line-height: 20px;
+  padding: 12px 14px;
+  background:white;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  .icon{
+    width: 28px;
+    height: 28px;
+  }
+`;
 
 function Account() {
-  return <Layout>account</Layout>;
+  return <Layout>
+    <Topbar>
+      <Icon name="left"/>
+      <span>记账</span>
+      <Icon/>
+    </Topbar>
+  </Layout>;
 }
 
-export default Account
+export default Account;
