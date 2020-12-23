d1c224287fb69147fd2f446ee6a459ac67ee9126
chili <1032569304@qq.com>
2020-12-23 14:33:48 +0800

    完成底部导航

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
 import styled from "styled-components";
-
+require('icons/detail.svg')
+require('icons/account.svg')
+require('icons/statistics.svg')
 const NavWrapper = styled.nav`
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
   line-height: 24px;
   font-size: 12px;
+  padding: 4px 0;
   >ul{
     display: flex;
     >li{
@@ -16,9 +19,11 @@ const NavWrapper = styled.nav`
         flex-direction: column;
         justify-content: center;
         align-items: center;
-        padding: 4px 0;
-        
-       } 
+        .icon{
+          width:32px;
+          height: 32px;
+        }
+       }  
      }
   }  
 `;
@@ -29,13 +34,23 @@ function Nav() {
       <ul>
         <li>
           <NavLink to="/detail">
+            <svg className="icon">
+              <use xlinkHref="#detail"/>
+            </svg>
             明细</NavLink>
         </li>
-        <li><NavLink to="/account">
+        <li>
+          <NavLink to="/account">
+            <svg className="icon">
+              <use xlinkHref="#account"/>
+            </svg>
           记账</NavLink>
         </li>
-        <li><NavLink to="/statistics">
-          统计</NavLink>
+        <li>
+          <NavLink to="/statistics">
+            <svg className="icon">
+              <use xlinkHref="#statistics"/>
+            </svg>统计</NavLink>
         </li>
       </ul>
     </NavWrapper>
