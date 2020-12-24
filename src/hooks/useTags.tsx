d15f8cac9da363820038b792154e5b1543a8df34
chili <1032569304@qq.com>
2020-12-24 22:09:32 +0800

    tags 数据持久化

---
+++ src/hooks/useTags.tsx
@@ -0,0 +1,47 @@
+import {useEffect, useState} from "react";
+import {createId} from "../lib/createId";
+import {useUpdate} from "./useUpdate";
+
+type Tag = {
+  id: number,
+  name: string,
+  value: string
+}
+const useTags = () => {
+  const [tags, setTags] = useState<Tag[]>([]);
+  // 组件挂载时执行，且只执行第一次 从 0 到有
+  useEffect(() => {
+    let localTags = JSON.parse(window.localStorage.getItem("tags") || "[]");
+    if (localTags.length === 0) {
+      localTags = [
+        {id: createId(), name: "奖金", value: "奖金"},
+        {id: createId(), name: "宠物", value: "宠物"},
+        {id: createId(), name: "工资", value: "工资"},
+        {id: createId(), name: "红包", value: "红包"},
+        {id: createId(), name: "餐饮", value: "餐饮"}
+      ];
+    }
+    setTags(localTags);
+  }, []);
+  // 自定义 hooks 第一次不执行 变化再执行
+  useUpdate(() => {
+    window.localStorage.setItem("tags", JSON.stringify(tags));
+  }, tags);
+
+  const addTag = () => {
+    const tagName = window.prompt("新标签的名称为");
+    const tagNames = tags.map(tag => tag.name);
+    if (tagName !== null) {
+      if (tagName === "") {
+        alert("标签名不能为空");
+      } else if (tagNames.indexOf(tagName) >= 0) {
+        alert("标签名重复");
+      } else {
+        setTags([...tags, {id: createId(), name: tagName, value: "其它"}]);
+      }
+    }
+  };
+  return {tags, addTag};
+};
+
+export {useTags}
--- src/views/Account/TagsSection.tsx
+++ src/views/Account/TagsSection.tsx
@@ -1,7 +1,7 @@
 import Icon from "components/Icon";
-import React, {useState} from "react";
+import React from "react";
 import styled from "styled-components";
-import {createId} from "../../lib/createId";
+import {useTags} from "hooks/useTags";
 
 const Wrapper = styled.section`
   >ul{
@@ -32,44 +32,21 @@ const Wrapper = styled.section`
 }
 `;
 
-type Tag = {
-  id: number,
-  name: string,
-  value: string
-}
-const X = [
-  {id: 1, name: "奖金", value: "奖金"},
-  {id: 2, name: "宠物", value: "宠物"},
-  {id: 3, name: "工资", value: "工资"},
-  {id: 4, name: "红包", value: "红包"},
-  {id: 5, name: "餐饮", value: "餐饮"}
-];
+
 type Props = {
   value: number,
   onChange: (selected: number) => void
 }
 
 const TagsSection: React.FC<Props> = (props) => {
-  const [tags, setTags] = useState<Tag[]>(X);
+  const {tags, addTag} = useTags();
   const selectedTagId = props.value;
   const onToggleTag = (tagId: number) => {
     if (selectedTagId !== tagId) {
       props.onChange(tagId);
     }
   };
-  const addTag = () => {
-    const tagName = window.prompt("新标签的名称为");
-    const tagNames = tags.map(tag => tag.name);
-    if (tagName !== null) {
-      if (tagName === "") {
-        alert("标签名不能为空");
-      } else if (tagNames.indexOf(tagName) >= 0) {
-        alert("标签名重复");
-      } else {
-        setTags([...tags, {id: createId(), name: tagName, value: "其它"}]);
-      }
-    }
-  };
+
   const getClass = (tagId: number) => selectedTagId === tagId ? "selected" : "";
   return (
     <Wrapper>
