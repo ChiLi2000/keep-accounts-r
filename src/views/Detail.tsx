270ec18cfda7d9ddbafee90c16a551125a5f70e5
chili <1032569304@qq.com>
2021-01-07 11:25:36 +0800

    Detail.tsx record 记录展示

--- src/views/Detail.tsx
+++ src/views/Detail.tsx
@@ -6,6 +6,8 @@ import {TimeWrapper} from "components/TimeWrapper";
 import {MyCategorySection} from "components/MyCategorySection";
 import styled from "styled-components";
 import Icon from "components/Icon";
+import {RecordItem, useRecords} from "../hooks/useRecords";
+import {useTags} from "../hooks/useTags";
 
 const RecordItemWrapper = styled.div`
   background: #ffff;
@@ -17,11 +19,11 @@ const RecordItemWrapper = styled.div`
       height: 38px;
       grid-column: 1;
       grid-row: 1 / span 2; 
-      margin-top: 6px;
+      margin-top: 2px;
     }
     >p{
       margin:0;
-      padding: 4px 10px;
+      padding: 2px 10px;
       display: flex;
       justify-content: space-between;
       align-items: center;
@@ -41,8 +43,25 @@ const RightContent = styled.span`
 `;
 
 function Detail() {
+  const {records} = useRecords();
+  const {getName} = useTags();
+  const hash: { [key: string]: RecordItem[] } = {};//  {'2020-05-11': [item, item], '2020-05-10': [item, item], '2020-05-12': [item, item, item, item]}
+  records.forEach(r => {
+    const key = moment(r.createdAt).format("MM月DD日");
+    if (!(key in hash)) {
+      hash[key] = [];
+    }
+    hash[key].push(r);
+  });
+  const array = Object.entries(hash).sort((a, b) => {
+    if (a[0] === b[0]) return 0;
+    if (a[0] > b[0]) return -1;
+    if (a[0] < b[0]) return 1;
+    return 0;
+  });
   const [x, setX] = useState(moment(new Date().toISOString()).format("YYYY-MM"));
   const styleTime = {"width": "110px", "borderRadius": "25px", "padding": "8px 16px"};
+
   return (
     <Layout>
       <TimeWrapper>
@@ -52,34 +71,19 @@ function Detail() {
                       style={styleTime}/>
       </TimeWrapper>
       <MyCategorySection slot='-'/>
-      <Header>
-        1月6日
-        <RightContent> 支出：￥3.00 收入：￥0.00 </RightContent>
-      </Header>
-      <RecordItemWrapper>
-        <Icon name="交通"/>
-        <p className="topItem">餐饮<span>-1</span></p>
-        <p className="bottomItem">备注<span>时间</span></p>
-      </RecordItemWrapper>
-      <RecordItemWrapper>
-        <Icon name="交通"/>
-        <p className="topItem">餐饮<span>-1</span></p>
-        <p className="bottomItem">备注<span>时间</span></p>
-      </RecordItemWrapper>
-      <Header>
-        1月6日
-        <RightContent> 支出：￥3.00 收入：￥0.00 </RightContent>
-      </Header>
-      <RecordItemWrapper>
-        <Icon name="交通"/>
-        <p className="topItem">餐饮<span>-1</span></p>
-        <p className="bottomItem">备注<span>时间</span></p>
-      </RecordItemWrapper>
-      <RecordItemWrapper>
-        <Icon name="交通"/>
-        <p className="topItem">餐饮<span>-1</span></p>
-        <p className="bottomItem">备注<span>时间</span></p>
-      </RecordItemWrapper>
+      {array.map(([date, records]) => <div key={date}>
+        <Header>
+          {date}
+          <RightContent> 支出：￥3.00 收入：￥0.00 </RightContent>
+        </Header>
+        {records.map(r => {
+          return <RecordItemWrapper key={r.idR}>
+            <Icon name={getName(r.tagId)}/>
+            <p className="topItem">{getName(r.tagId)}<span>{r.category === "-" ? (-r.amount) : (+r.amount)}</span></p>
+            <p className="bottomItem">{r.note}<span>{(r.createdAt).slice(11)}</span></p>
+          </RecordItemWrapper>;
+        })}
+      </div>)}
 
     </Layout>
   );
