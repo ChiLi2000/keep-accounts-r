fee7e14e72e708b94f4ced6d1a353fc3609498e8
chili <1032569304@qq.com>
2021-01-09 20:51:57 +0800

    解决改变原记录带来的问题

--- src/components/ChartLine.tsx
+++ src/components/ChartLine.tsx
@@ -4,14 +4,15 @@ import "echarts/lib/component/tooltip";
 import {RecordItem} from "hooks/useRecords";
 import moment from "moment";
 import {numberFilter} from "lib/numberFilter";
+import clone from "../lib/clone";
 
 const echarts = require("echarts");
 type Props = {
   axis: number
-  value:  RecordItem[]
+  value: RecordItem[]
 }
 const ChartLine: React.FC<Props> = (props) => {
-  const axis=props.axis
+  const axis = props.axis;
   const value = props.value;
   const options = {
     title: {
@@ -63,28 +64,38 @@ const ChartLine: React.FC<Props> = (props) => {
       data: [] as any
     }]
   };
+
   useEffect(() => {
-    changeData()
+    options.xAxis.data = [];
+    options.series[0].data = [];
+    changeData();
     const chart = echarts.init(document.getElementById("chartLine") as HTMLElement);
     chart.setOption(options);
-  }, [ options]);
+  }, [options]);
   const changeData = () => {
-    for (let i = 0; i < value.length - 1; i++) {
-      for (let j = i + 1; j < value.length; j++) {
-        if ((value[i].createdAt).slice(0,10) === (value[j].createdAt).slice(0,10)) {
-          value[i].amount += value[j].amount;
-          value.splice(j, 1);
+    const newValue = clone(value);
+    for (let i = 0; i < newValue.length - 1; i++) {
+      for (let j = i + 1; j < newValue.length; j++) {
+        if ((newValue[i].createdAt).slice(0, 10) === (newValue[j].createdAt).slice(0, 10)) {
+          newValue[i].amount += value[j].amount;
+          // newValue.splice(j, 1);
+
         }
       }
     }
+    console.log("外面的");
+    for (let i = 0; i < newValue.length; i++) {
+      console.log(newValue[i]);
+      console.log("里面的");
+    }
     for (let i = 1; i <= axis; i++) {
       let t = 0;
-      for (let j = 0; j < value.length; j++) {
-        if(i===moment(value[j].createdAt).date()){
+      for (let j = 0; j < newValue.length; j++) {
+        if (i === moment(newValue[j].createdAt).date()) {
           t++;
-          options.xAxis.data.push(i+'日')
-          options.series[0].data.push(numberFilter(value[j].amount))
-          break
+          options.xAxis.data.push(i + "日");
+          options.series[0].data.push(numberFilter(newValue[j].amount));
+          break;
         }
       }
       if (t === 0) {
