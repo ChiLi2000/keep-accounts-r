691d2f8ffaf03f8658462afad01b4c1ee3051ff6
chili <1032569304@qq.com>
2021-01-16 21:52:55 +0800

    添加二维码扫描

--- src/index.tsx
+++ src/index.tsx
@@ -8,3 +8,14 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+if(document.documentElement.clientWidth>500){
+  const img = document.createElement('img');
+  img.src = './qrcode.png'
+  img.style.position = 'fixed'
+  img.style.left='50%'
+  img.style.top='50%'
+  img.style.transform='translate(-50%,-50%)'
+  img.style.boxShadow='0 0 10px rgba(0,0,0,0.25)'
+  document.body.appendChild(img)
+}
